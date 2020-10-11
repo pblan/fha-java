5bcb8cc0981a255cf863a381321de41acc56d3a4
Patrick Gustav Blaneck <patrick.blaneck@rwth-aachen.de>
2020-10-11 16:19:34 +0200

    h01 done

--- ha01/KomplexeZahl.java
+++ ha01/KomplexeZahl.java
@@ -1,42 +1,81 @@
 import java.util.Arrays;
 
+/**
+ * Describes a complex number z with it's corresponding real and imaginary parts
+ * Handles addition, multiplication, square root, absolute value
+ */
 public class KomplexeZahl {
     private double real;
     private double imaginary;
 
+    /**
+     * Creates a new KomplexeZahl
+     * @param real
+     * @param imaginary
+     */
     public KomplexeZahl(double real, double imaginary) {
         this.real = real;
         this.imaginary = imaginary;
     }
 
+    /**
+     * 
+     * @return real part of current KomplexeZahl (real value)
+     */
     public double getReal() {
         return this.real;
     }
 
+    /**
+     * 
+     * @param real part to set (real value)
+     */
     public void setReal(double real) {
         this.real = real;
     }
 
+    /**
+     * 
+     * @return imaginary part of current KomplexeZahl (real value)
+     */
     public double getImaginary() {
         return this.imaginary;
     }
 
+    /**
+     * 
+     * @param imaginary part to set (real value)
+     */
     public void setImaginary(double imaginary) {
         this.imaginary = imaginary;
     }
 
+    /**
+     * Adds a given KomplexeZahl onto the current KomplexeZahl
+     * @param KomplexeZahl to be added
+     */
     public void addiere(KomplexeZahl z) {
         this.real += z.real;
         this.imaginary += z.imaginary;
     }
 
+    /**
+     * Multiplies a given KomplexeZahl onto the current KomplexeZahl
+     * z * z' = (x + iy) * (x' + iy') = (xx' - yy') + i(xy' - x'y)
+     * @param KomplexeZahl to be multiplied 
+     */
     public void multipliziere(KomplexeZahl z) {
-        double real = (this.real * z.real) - (this.imaginary * z.imaginary);
+        double real = (this.real * z.real) - (this.imaginary * z.imaginary); 
         double imaginary = (this.real * z.imaginary) + (z.real * this.imaginary);
         this.real = real;
         this.imaginary = imaginary;
     }
 
+    /**
+     * Returns absolute value of current KomplexeZahl
+     * z := x + iy => abs(z) = sqrt(x^2 + y^2)
+     * @return absolute value of current KomplexeZahl
+     */
     public double getBetrag() {
         return Math.sqrt(this.real*this.real + this.imaginary*this.imaginary);
     }
@@ -49,6 +88,11 @@ public class KomplexeZahl {
         return res;
     }
 
+    /**
+     * Returns both square roots of the current KomplexeZahl
+     * Returns 0 iff current KomplexeZahl equals 0
+     * @return array containing the two square roots of current KomplexeZahl
+     */
     public KomplexeZahl[] getWurzel() {
         double abs, phi, real, imaginary;
         KomplexeZahl[] res = new KomplexeZahl[2];
@@ -58,29 +102,38 @@ public class KomplexeZahl {
             res[1] = new KomplexeZahl(0.0, 0.0);
             return res;
         }
+                                                                        // z:= x + iy; z1 := sqrt(z); z2 := conjugated z1
+        abs = this.getBetrag();                                         // r := abs(z)
+        phi = Math.atan2(this.imaginary, this.real);                    // phi := arc length of z
+        real = Math.sqrt(abs) * Math.cos(phi/2);                        // R(z1) = sqrt(r) * cos(phi/2) <= Moivre for k=0
+        imaginary = Math.sqrt(abs) * Math.sin(phi/2);                   // I(z1) = sqrt(r) * sin(phi/2) <= Moivre for k=0
 
-        abs = this.getBetrag();
-        phi = Math.atan2(this.imaginary, this.real);
-        System.out.println(phi);
-        real = Math.sqrt(abs) * Math.cos(phi/2);
-        imaginary = Math.sqrt(abs) * Math.sin(phi/2);
+        res[0] = new KomplexeZahl(real, imaginary);                     // res << z1
 
-        res[0] = new KomplexeZahl(real, imaginary);
+        real = Math.sqrt(abs) * Math.cos((phi + 2* Math.PI)/2);         // R(z2) = sqrt(r) * cos((phi + 2pi)/2) <= Moivre for k=1
+        imaginary = Math.sqrt(abs) * Math.sin((phi + 2* Math.PI)/2);    // I(z2) = sqrt(r) * sin((phi + 2pi)/2) <= Moivre for k=1
 
-        real = Math.sqrt(abs) * Math.cos((phi + 2* Math.PI)/2);
-        imaginary = Math.sqrt(abs) * Math.sin((phi + 2* Math.PI)/2);
-
-        res[1] = new KomplexeZahl(real, imaginary);
+        res[1] = new KomplexeZahl(real, imaginary);                     // res << z2
 
         return res;
     }
 
+    /**
+     * Returns new KomplexeZahl which is the sum of current KomplexeZahl and given KomplexeZahl
+     * @param KomplexeZahl to be added
+     * @return sum of current KomplexeZahl and given KomplexeZahl
+     */
     public KomplexeZahl getSumme(KomplexeZahl z) {
         KomplexeZahl res = new KomplexeZahl(this.real, this.imaginary);
         res.addiere(z);
         return res;
     }
 
+    /**
+     * Returns new KomplexeZahl which is the product of current KomplexeZahl and given KomplexeZahl
+     * @param KomplexeZahl to be multiplied with
+     * @return product of current KomplexeZahl and given KomplexeZahl
+     * */
     public KomplexeZahl getProdukt(KomplexeZahl z) {
         KomplexeZahl res = new KomplexeZahl(this.real, this.imaginary);
         res.multipliziere(z);
